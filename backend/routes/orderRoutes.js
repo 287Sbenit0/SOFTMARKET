071efc7eec4b4706d400718a61200015cbb00593
BnyDev <115947408+287Sbenit0@users.noreply.github.com>
2024-06-11 21:29:18 +0200

    Delete backend directory

--- backend/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-const { verifyToken } = require('../middleware/authJwt');
-
-router.post('/', verifyToken, orderController.create);
-
-module.exports = router;
